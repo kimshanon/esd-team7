ef9962e583d57836d706fd704837432adbfe4b6d
kimshanon <ksa.ganap.2023@smu.edu.sg>
2025-03-10 14:07:13 +0800

    Update foodstall.sql

--- foodstall.sql
+++ foodstall.sql
@@ -13,3 +13,16 @@ VALUES
   ('Triplets', '70 Stamford Road #B1-24A Li Ka Shing Library Building, Singapore 178901'),
   ('Khoon', '90 Stamford Road #01-72 School of Economics / School of Computing and Information Systems 2 Building Singapore 178903'),
   ('Canteen Bistro', '80 Stamford Road #B1-61 School of Computing and Information Systems Building, Singapore 178902');
+
+CREATE TABLE FoodMenu (
+  foodID INT AUTO_INCREMENT PRIMARY KEY,
+  stallID INT,
+  foodName VARCHAR(255),
+  foodPrice INT,
+  foodDescription VARCHAR(255),
+  FOREIGN KEY (stallID) REFERENCES FoodStall(stallID)
+);
+
+
+
+
