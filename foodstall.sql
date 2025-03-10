df33cb28a745cee797470539fa9c28a2f59a4c79
kimshanon <ksa.ganap.2023@smu.edu.sg>
2025-03-10 15:37:11 +0800

    Update foodstall.sql

--- foodstall.sql
+++ foodstall.sql
@@ -24,39 +24,39 @@ CREATE TABLE FoodMenu (
 
 INSERT INTO FoodMenu (stallID, menuName, menuPrice)
 VALUES
-    (1, 'Fresh Milk Cheese Omelette Curry Rice', 6.0)
-    (1, 'Original Chicken Katsu Curry Rice', 6.0)
-    (1, 'Mayo Chicken Katsu Curry Rice', 6.0)
-    (1, 'Spicy Mayo Chicken Katsu Curry Rice', 6.0)
-    (1, 'Cheese Mayo Chicken Katsu Curry Rice', 6.0)
-    (1, 'Black Vinegar Mayo Chicken Katsu Curry Rice', 6.0)
-    (1, 'Tartar Mayo Chicken Katsu Curry Rice', 6.0)
-    (1, 'Truffle Mayo Curry Rice', 6.0)
-    (1, 'Fish Katsu Curry Rice', 6.5)
-    (1, 'Salmon Katsu Curry Rice', 7.0)
-    (1, 'Ebi Katsu Curry Rice', 7.0)
-    (1, 'Takoyaki Curry Rice', 6.5)
+    (1, 'Fresh Milk Cheese Omelette Curry Rice', 6.0),
+    (1, 'Original Chicken Katsu Curry Rice', 6.0),
+    (1, 'Mayo Chicken Katsu Curry Rice', 6.0),
+    (1, 'Spicy Mayo Chicken Katsu Curry Rice', 6.0),
+    (1, 'Cheese Mayo Chicken Katsu Curry Rice', 6.0),
+    (1, 'Black Vinegar Mayo Chicken Katsu Curry Rice', 6.0),
+    (1, 'Tartar Mayo Chicken Katsu Curry Rice', 6.0),
+    (1, 'Truffle Mayo Curry Rice', 6.0),
+    (1, 'Fish Katsu Curry Rice', 6.5),
+    (1, 'Salmon Katsu Curry Rice', 7.0),
+    (1, 'Ebi Katsu Curry Rice', 7.0),
+    (1, 'Takoyaki Curry Rice', 6.5);
 
 INSERT INTO FoodMenu (stallID, menuName, menuPrice)
 VALUES
-    (2, 'Plain Waffle', 1.8)
-    (2, 'Peanut Waffle', 2.3)
-    (2, 'Chocolate Waffle', 2.3)
-    (2, 'Kaya Waffle', 2.3)
-    (2, 'Cream Cheese Waffle', 2.3)
-    (2, 'Strawberry Waffle', 2.3)
-    (2, 'Blueberry Waffle', 2.3)
-    (2, 'Slice Cheese Waffle', 2.3)
-    (2, 'Crunchy Peanut Waffle', 2.8)
-    (2, 'Chocolate Oreo Waffle', 2.8)
-    (2, 'Walnut Chocolate Waffle', 2.8)
-    (2, 'Almond Chocolate Waffle', 2.8)
-    (2, 'Chicken Ham & Cheese Waffle', 2.8)
-    (2, 'Oreo Cheese Waffle', 2.8)
-    (2, 'Almond Chocolate Waffle', 2.8)
-    (2, 'Blueberry Cheese Waffle', 2.8)
-    (2, 'Cranberry Cheese Waffle', 2.8)
-    (2, 'Redbean Waffle', 2.8)
+    (2, 'Plain Waffle', 1.8),
+    (2, 'Peanut Waffle', 2.3),
+    (2, 'Chocolate Waffle', 2.3),
+    (2, 'Kaya Waffle', 2.3),
+    (2, 'Cream Cheese Waffle', 2.3),
+    (2, 'Strawberry Waffle', 2.3),
+    (2, 'Blueberry Waffle', 2.3),
+    (2, 'Slice Cheese Waffle', 2.3),
+    (2, 'Crunchy Peanut Waffle', 2.8),
+    (2, 'Chocolate Oreo Waffle', 2.8),
+    (2, 'Walnut Chocolate Waffle', 2.8),
+    (2, 'Almond Chocolate Waffle', 2.8),
+    (2, 'Chicken Ham & Cheese Waffle', 2.8),
+    (2, 'Oreo Cheese Waffle', 2.8),
+    (2, 'Almond Chocolate Waffle', 2.8),
+    (2, 'Blueberry Cheese Waffle', 2.8),
+    (2, 'Cranberry Cheese Waffle', 2.8),
+    (2, 'Redbean Waffle', 2.8);
 
 INSERT INTO FoodMenu (stallID, menuName, menuPrice) 
 VALUES 
@@ -72,7 +72,7 @@ VALUES
 (4, 'Crispy Chicken', 14, ''), 
 (4, 'Crispy Fish', 14, ''), 
 (4, 'Pulled Pork', 16, ''), 
-(4, 'Wagyu Beef', 18,'')
+(4, 'Wagyu Beef', 18,'');
 
 
 
